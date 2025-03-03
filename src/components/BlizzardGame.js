a782cca2c41b6de2212dde464d9e6dd855b94a09
Laura <larppa_98@hotmail.com>
2025-03-03 23:44:23 +0200

    Disabled touch screen on the game, lagged too much

--- src/components/BlizzardGame.js
+++ src/components/BlizzardGame.js
@@ -27,7 +27,7 @@ const BlizzardGame = (p) => {
 
     moveDown() {
       this.y += this.speed;
-      this.x += p.random(-1, 1); // Random horizontal movement for realness
+      this.x += p.random(-1, 1); //Random horizontal movement
       if (this.x > p.width) this.x = 0;
     }
 
@@ -39,35 +39,25 @@ const BlizzardGame = (p) => {
   }
 
   p.preload = () => {
-    flowerImg = p.loadImage('/images/floweri.png'); // Loading flower image
+    flowerImg = p.loadImage('/images/floweri.png'); //Loading flower image
   };
 
   p.setup = () => {
     p.createCanvas(640, 320);
     pileHeights = new Array(p.width).fill(0);
   };
-
-  /*Add touch event handlers here*/
-  p.touchStarted = () => {
-    sweeping = true;
-    return false; // Prevent default touch behavior (so like scrolling)
-  };
-
-  p.touchEnded = () => {
-    sweeping = false;
-  };
-
+  
   p.draw = () => {
     p.background(171, 231, 254);
 
-    // Adding new snowflakes
+    //Add new snowflakes
     if (p.frameCount % 10 === 0) {
       foregroundSnow.push(new Snowflake(p.random(WHITE_COLORS), p.random(5, 7), p.random(8, 15), p.random(200, 255)));
       midgroundSnow.push(new Snowflake(p.random([...BLUE_COLORS, ...WHITE_COLORS]), p.random(4, 6), p.random(5, 10), p.random(100, 200)));
       backgroundSnow.push(new Snowflake(p.random(BLUE_COLORS), p.random(3, 5), p.random(3, 7), p.random(50, 150)));
     }
 
-    // Draw and move snowflakes
+    //To draw and move snowflakes
     for (let snow of backgroundSnow) {
       snow.moveDown();
       snow.draw();
@@ -84,7 +74,7 @@ const BlizzardGame = (p) => {
       snow.moveDown();
       snow.draw();
 
-      /*Check if snowflake hits the sill*/
+      //Checking if snowflake hits the sill
       if (snow.y >= p.height - 20) {
         let xDiscrete = p.floor(snow.x);
         pileHeights[xDiscrete] += snow.radius;
@@ -99,20 +89,20 @@ const BlizzardGame = (p) => {
       }
     }
 
-    // Draw piled snow
+    //Drawing piled snow
     for (let snowData of piledSnowList) {
       p.fill(...snowData.color, snowData.opacity);
       p.noStroke();
       p.circle(snowData.x, snowData.y, snowData.radius * 2);
     }
 
-    // Draw window
+    //Drawing window
     p.fill(196, 164, 132);
     p.rect(0, p.height - 20, p.width, 20);
     p.rect(0, 0, p.width, 20);
     p.rect(p.height - 20, 0, 20, p.width);
-
-    // Add the flower
+    
+    //Adding the flower.png
     p.image(flowerImg, p.width - 550, p.height - 90, 110, 110);
 
     // Draw curtains
@@ -121,28 +111,16 @@ const BlizzardGame = (p) => {
     p.rect(0, 0, 60, p.height); // Right curtain
     p.rect(p.width - 60, 0, 60, p.height); // Left curtain
 
-    // Sweeping logic
+    //Sweeping logic
     if (sweeping) {
-      let touchPos;
+      let mousePos = p.createVector(p.mouseX, p.mouseY);
+      let sweepRadius = 20;
 
-      // Use mouse position if on desktop
-      if (p.mouseIsPressed) {
-        touchPos = p.createVector(p.mouseX, p.mouseY);
-      }
-      // Use touch position if on mobile
-      else if (p.touches.length > 0) {
-        touchPos = p.createVector(p.touches[0].x, p.touches[0].y);
-      }
-
-      if (touchPos) {
-        let sweepRadius = 20;
-
-        for (let snowData of piledSnowList.slice()) {
-          let distance = p.dist(touchPos.x, touchPos.y, snowData.x, snowData.y);
-          if (distance <= sweepRadius) {
-            piledSnowList.splice(piledSnowList.indexOf(snowData), 1);
-            pileHeights[snowData.x] -= snowData.radius;
-          }
+      for (let snowData of piledSnowList.slice()) {
+        let distance = p.dist(mousePos.x, mousePos.y, snowData.x, snowData.y);
+        if (distance <= sweepRadius) {
+          piledSnowList.splice(piledSnowList.indexOf(snowData), 1);
+          pileHeights[snowData.x] -= snowData.radius;
         }
       }
     }
