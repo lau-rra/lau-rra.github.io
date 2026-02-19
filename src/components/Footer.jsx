2ca4dc1ba6d8714415317efb3663866b8dddf40f
Laura <larppa_98@hotmail.com>
2026-02-19 17:06:51 +0900

    Photo updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ function Footer() {
   return (
     <>
     <div className="footer">
-      <p>Updated 29-03-2025 |
+      <p>Updated 18-02-2026 |
         <Link className="link" target="_blank" to="https://www.linkedin.com/in/lauralumijarvi/">
           {/*If I want to add the icon later: <img
             src="/images/linkedin.svg"
             alt="Linkedin logo"
             className="linkedin"
           />*/}
-          LinkedIn
+          Linkedin
         </Link>
         | Have a nice day&lt;3
       </p>
